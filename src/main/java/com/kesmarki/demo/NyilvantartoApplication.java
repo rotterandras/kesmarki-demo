af382d18362590d553b0e49ce7a12e8f17475283
randras <rotterandras@gmail.com>
2024-01-22 15:30:14 +0100

    Initial commit

---
+++ src/main/java/com/kesmarki/demo/NyilvantartoApplication.java
@@ -0,0 +1,15 @@
+package com.kesmarki.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyilvantartoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyilvantartoApplication.class, args);
+	}
+
+}
